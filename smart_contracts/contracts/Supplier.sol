41358080acf2d8eb0316b155a567fbbc38623e35
Nirbhay Jnanendra <nirbhayj12@gmail.com>
2020-06-03 13:23:45 +0530

    Create Supplier.sol

---
+++ smart_contracts/contracts/Supplier.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.5.0;
+
+import "./Entities.sol";
+
+contract Supplier {
+    using Entities for Entities.Entity;
+    
+    event SupplierAdded(address indexed account);
+    event SupplierRemoved(address indexed account);
+    
+    Entities.Entity private supp; // Define a struct 'supp'
+    
+    constructor() public {
+        _addSupp(msg.sender);
+    }
+    
+    modifier onlySupp() {
+        require(isSupp(msg.sender));
+        _;
+    }
+    
+    function isSupp(address acc) public view returns  (bool) {
+        return supp.own(acc);
+    }
+    
+    function addSupp (address acc) public onlySupp {
+        _addSupp(acc);
+    }
+    
+    function remSupp() public {
+        _remSupp(msg.sender);
+    }
+    
+    function _addSupp (address acc) internal {
+        supp.add(acc);
+        emit SupplierAdded(acc);
+    }
+    
+    function _remSupp (address acc) internal {
+        supp.rem(acc);
+        emit SupplierRemoved(acc);
+    }
+}
