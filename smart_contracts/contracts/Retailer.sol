2114274a927bc10db8606caf13441df8a9b4fcf1
Nirbhay Jnanendra <nirbhayj12@gmail.com>
2020-06-03 13:25:55 +0530

    Create Retailer.sol

---
+++ smart_contracts/contracts/Retailer.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.0;
+
+import "./Entities.sol";
+
+contract Retailer {
+    using Entities for Entities.Entity;
+    
+    event RetailerAdded(address indexed account);
+    event RetailerRemoved(address indexed account);
+    
+    Entities.Entity private retl; 
+    
+    constructor() public {
+        _addRetl(msg.sender);
+    }
+    
+    modifier onlyRetl() {
+        require(isRetl(msg.sender));
+        _;
+    }
+    
+    function isRetl(address acc) public view returns (bool) {
+        return retl.own(acc);
+    }
+    
+    function addRetl (address acc) public onlyRetl {
+        _addRetl(acc);
+    }
+    
+    function remRetl() public {
+        _remRetl(msg.sender);
+    }
+    
+    function _addRetl (address acc) internal {
+        retl.add(acc);
+        emit RetailerAdded(acc);
+    }
+    
+    function _remRetl (address acc) internal {
+        retl.rem(acc);
+        emit RetailerRemoved(acc);
+    }
+}
+
+
