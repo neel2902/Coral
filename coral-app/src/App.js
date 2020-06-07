49db4b81f354ed7ecbda7dd57cea3fed6b8b75f7
neel2902 <nilkamalthakuria2902@gmail.com>
2020-06-08 03:49:40 +0530

    committing to 2nd branch

--- coral-app/src/App.js
+++ coral-app/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import login from './auth/login';
+import signup from './auth/signup';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+        <Switch>
+          <Route exact path="/" component={login}/>
+          <Route exact path="/signup" component={signup}/>
+        </Switch>
+    </Router>
     </div>
   );
 }
