2dbd576a75e804a80b9177668e58014872176dad
Nilkamal Thakuria <nilkamalthakuria2902@gmail.com>
2020-06-17 12:41:40 +0530

    Update deploy.js

--- smart_contracts/deploy.js
+++ smart_contracts/deploy.js
@@ -1,28 +1,3 @@
-// const { interface, object: bytecode } = require('./compile'); 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const Web3 = require('web3');
-// const mnemonic = "reward extend alien rival slice only timber skin oven critic sorry reflect";
-
-// const provider = new HDWalletProvider(
-//   mnemonic,
-//   'https://rinkeby.infura.io/v3/1293ed76ae6a434980e12ff7716a6ead'
-// );
-// const web3 = new Web3(provider);
-
-// const deploy = async () => {
-//   const accounts = await web3.eth.getAccounts();
-
-//   console.log('Attempting to deploy from account', accounts[0]);
-
-//   const result = await new web3.eth.Contract(interface)
-//     .deploy({ data: "0x" + bytecode })
-//     .send({ from: accounts[0] });
-
-//   console.log('Contract deployed to', result.options.address);
-// };
-// deploy();
-
-
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
